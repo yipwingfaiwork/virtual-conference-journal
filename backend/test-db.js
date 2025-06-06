1add2ce10ed4430b29b5c25948cf8cba7bc7933b
yipwingfaiwork <yipwingfaiwork@gmail.com>
2025-06-06 23:03:13 +0800

    Fix SSL connection and local server setup

--- backend/test-db.js
+++ backend/test-db.js
@@ -12,7 +12,7 @@ async function testConnection() {
       database: process.env.DB_NAME,
       port: process.env.DB_PORT,
       ssl: {
-        ca: require('fs').readFileSync('./certs/BaltimoreCyberTrustRoot.crt.pem')
+        ca: require('fs').readFileSync('C:\\Github\\virtual-conference-journal\\backend\\certs\\DigiCertGlobalRootCA.crt.pem')
       }
     });
     console.log('Connection established, testing query...');
